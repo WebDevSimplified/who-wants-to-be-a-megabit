62b4bb5e957c70e469d9984a8880b33a4406492b
Matthias Zettler <info@matthias-zettler.de>
2022-02-04 18:14:37 +0100

    Changed import from app.css to App.css, because it fails on Linux due to case-sensitive filesystem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import "./app.css"
+import "./App.css"
 import Question from "./components/Question"
 import Sidebar from "./components/Sidebar"
 import QUESTION_LIST from "./questions/Clement"
